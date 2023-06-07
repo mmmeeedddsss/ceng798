a79a931abad362fc6d26155419c29a8055696905
Mert Tunc <mert.tunc@udemy.com>
2023-06-07 16:28:11 +0300

    Initial version of python notebook

--- grovers_string_search.py
+++ grovers_string_search.py
@@ -29,28 +29,26 @@ def set_inputs(qc, inp_w, inp_p):
 
 def initialize_s(qc):
     """Apply a H-gate to 'qubits' in qc"""
-    qc.h(s_list)
+    qc.h(s)
 
 
 def oracle(qc: QuantumCircuit, start_pos):
     # if the start_pos'th bit is equal on w and p, then increase
-    format_string = "{:0" + str(len(s_chunks[0])) + "b}"
+    format_string = "{:0" + str(len(s)) + "b}"
     pos_in_binary = format_string.format(start_pos)
     qc.barrier(label=f"start {pos_in_binary}")
-    for offset, s in enumerate(s_chunks):
-        pos_in_binary = format_string.format(start_pos + offset)
 
-        # for k, pos in enumerate(pos_in_binary):  # 10
-        flipped_s = []
+    flipped_s = []
+    # Here, we would need a anc line to or the cases then the s1 should be 1
+    # there are n-m-1 such cases, w0 = p0, w1=p0 ...
+    for k, pos in enumerate(pos_in_binary):  # 10
+        if pos == "0":
+            flipped_s.append(s[k])
+            qc.x(s[k])
+
+    for offset, match_bit in enumerate(bit_matches):
         flipped_w = []
         flipped_p = []
-
-        # Here, we would need a anc line to or the cases then the s1 should be 1
-        # there are n-m-1 such cases, w0 = p0, w1=p0 ...
-        for k, pos in enumerate(pos_in_binary):  # 10
-            if pos == "0":
-                flipped_s.append(s[k])
-                qc.x(s[k])
 
         if inp_p[0 + offset] == "0":
             flipped_p.append(p[0 + offset])
@@ -58,18 +56,6 @@ def oracle(qc: QuantumCircuit, start_pos):
 
             flipped_w.append(w[0 + start_pos + offset])
             qc.x(w[0 + start_pos + offset])
-
-        """
-        qc.z(ancs[k])
-        qc.mct([s[k], w[start_pos + offset], p[0 + offset]], [ancs[k]])
-        qc.z(ancs[k])
-        """
-
-        """
-        qc.h(ancs)
-        qc.mcx([s[k], w[start_pos + offset], p[0 + offset]], ancs[k])
-        qc.h(ancs)
-        """
 
         qc.mcx(list(s) + [w[start_pos + offset], p[0 + offset]], bit_matches[offset])
 
@@ -79,51 +65,41 @@ def oracle(qc: QuantumCircuit, start_pos):
             qc.x(flipped_w)
         if len(flipped_p):
             qc.x(flipped_p)
-        if len(flipped_s):
-            qc.x(flipped_s)
+
+    if len(flipped_s):
+        qc.x(flipped_s)
     qc.barrier(label=f"end {pos_in_binary}")
 
 
 def diffusion(qc: QuantumCircuit):
     """Apply a diffusion circuit to the register 'qubits' in qc"""
-    for reg in s_chunks:
-        qc.h(reg)
-        qc.x(reg)
+    qc.h(s)
+    qc.x(s)
 
-    qc.mcx(s_list, match)
+    qc.mcx(s, match)
     qc.z(match)
 
-    for reg in s_chunks:
-        qc.x(reg)
-        qc.h(reg)
+    qc.x(s)
+    qc.h(s)
 
 
-def measurement_s(grover_circuit, num_shots=1000):
-    num_bits = len(s_list)
+def measurement_s(qc):
+    num_bits = len(s)
     meas = QuantumCircuit(num_bits, num_bits)
     meas.measure(list(range(num_bits)), list(range(num_bits)))
-
-    backend = provider.backend.ibmq_qasm_simulator  # ibmq_qasm_simulator
-    grover_circuit.compose(meas, inplace=True, qubits=s_list)
-    result = backend.run(transpile(grover_circuit, backend), shots=num_shots).result()
-    counts = result.get_counts(grover_circuit)
-
-    print("Done!")
-    plot_histogram(counts)
-
-    aggregated_counts = {}
-    for k, v in counts.items():
-        agg_k = k[-num_sbits:]
-        if agg_k in aggregated_counts:
-            aggregated_counts[agg_k] += v
-        else:
-            aggregated_counts[agg_k] = v
-    plot_histogram(aggregated_counts)
+    qc.compose(meas, inplace=True, qubits=s)
 
 
-inp_w = "11011011"
-inp_p = "11"
+def execute(qc, num_shots=1000):
+    backend = provider.backend.ibmq_qasm_simulator
+    result = backend.run(transpile(qc, backend), shots=num_shots).result()
+    return result.get_counts(qc)
 
+
+counts = execute(qc)
+
+inp_w = "11001000"
+inp_p = "1000"
 
 n = len(inp_w)
 m = len(inp_p)
@@ -140,20 +116,20 @@ print(num_sbits)
 # Number of s'es will be len(m)
 # Number of bits in s'es will be num_sbits
 
-s_list = QuantumRegister(num_sbits * m, 's_list')
-s_chunks = list(list_to_chunks(s_list, num_sbits))
+s = QuantumRegister(num_sbits, 's_list')
+# s_chunks = list(list_to_chunks(s_list, num_sbits))
 w = QuantumRegister(n, 'w')
 p = QuantumRegister(m, 'p')
 bit_matches = QuantumRegister(m, 'bit_matches')
 match = QuantumRegister(1, 'match')
 
-qc = QuantumCircuit(s_list, w, p, bit_matches, match)
+qc = QuantumCircuit(s, w, p, bit_matches, match)
 set_inputs(qc, inp_w, inp_p)
 initialize_s(qc)
 
-num_repetitions = int(math.sqrt(2 ** (num_sbits * m)))
+num_repetitions = int(math.sqrt(2 ** (num_sbits)))
 print(f"Circuit depth is {num_repetitions}")
-for repetition in range(num_repetitions):  # todo 2 needs to be altered
+for repetition in range(num_repetitions):  # num_repetitions
     for start_pos in range(n - m + 1):
         oracle(qc, start_pos)
         qc.mcx(bit_matches, match)
@@ -161,6 +137,9 @@ for repetition in range(num_repetitions):  # todo 2 needs to be altered
     diffusion(qc)
 
 measurement_s(qc)
+
+counts = execute(qc)
+plot_histogram(counts)
 print(qc.depth())
 
 qc.draw(output='mpl', scale=0.5)
